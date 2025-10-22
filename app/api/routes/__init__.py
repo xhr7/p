8059b78e090d30286bd1140df5fe98cd5ca361bf
Rahaf Masmali <rahafmasmali@Rahafs-MacBook-Pro.local>
2025-10-22 13:00:16 +0300

    kdmsl

--- app/api/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-# Empty init file to make the directory a package
-
-# Made with Bob
--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import services for convenient imports
-from app.services import user, item
-
-__all__ = ["user", "item"]
-
-# Made with Bob
